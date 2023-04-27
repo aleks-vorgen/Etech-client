2479ba9bc2ad3dc111dba7591c649bbb02771c1c
skinner.x8 <Cezar21044@gmail.com>
2023-04-27 16:57:03 +0300

    fff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <h1>Отримайте щоденну знижку на покупки від 1000 гривень! Оновлення акції кожен день!</h1>
         </div>
 
-        <div className='h-[100px] bg-black mb-[50px] '>
+        <div className=' w-[1100px] h-[100px] bg-black mb-[50px] m-auto'>
             <div className='flex items-center justify-center uppercase text-[11px] text-center weight-normal tracking-[0.01rem] '> 
                 <><img src={Refund} alt="RefundHeader" /> <h1 className='mr-[35px] ml-2'>Повернення протягом 30 днів</h1></>
                 <><img src={Shipping} alt="ShippingHeader" /> <h1 className='mr-[35px] ml-2'>безкоштовна доставка</h1></>
