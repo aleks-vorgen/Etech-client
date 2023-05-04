0c73be0f36dd069d9a889ab4917c545488487514
ALexus <sumskoj01@gmail.com>
2023-05-04 22:44:08 +0300

    fetching data from gcp to netlify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,11 @@ import axios from 'axios'
 
 
 export function HomePage() {
-  let data;
   const getDataProducts = () => {
     axios.get('https://etech-5fydkirpga-lm.a.run.app/categories/all')
     //axios.get('http://localhost:8080/categories/all')
     .then (res => {
-      data = res.data
+      console.log(res.data)
     })
     .catch (error => {
       console.log(error);
@@ -19,9 +18,6 @@ export function HomePage() {
     <div style={{margin: '0px 50px'}}>
     <h2>Головна</h2>
     <button style={{border: '1px solid black', padding: '5px 10px'}} onClick={getDataProducts}> нажми</button>
-      <div>
-        { data }
-      </div>
     </div>
   )
 }
