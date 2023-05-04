548fb0fab368c57231c835cf6284015df9d75951
ALexus <sumskoj01@gmail.com>
2023-05-04 23:11:24 +0300

    fetching data from gcp to netlify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,21 +3,40 @@ import axios from 'axios'
 
 
 export function HomePage() {
-  const getDataProducts = () => {
-    axios.get('https://etech-5fydkirpga-lm.a.run.app/categories/all')
-    //axios.get('http://localhost:8080/categories/all')
-    .then (res => {
-      console.log(res.data)
-    })
-    .catch (error => {
-      console.log(error);
-    });
+  const getProductData = () => {
+    axios.get('https://etech-5fydkirpga-lm.a.run.app/products/all')
+        //axios.get('http://localhost:8080/categories/all')
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
+
+  function getUserData() {
+    axios.get('https://etech-5fydkirpga-lm.a.run.app/users/all')
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
   }
 
   return (
-    <div style={{margin: '0px 50px'}}>
-    <h2>Головна</h2>
-    <button style={{border: '1px solid black', padding: '5px 10px'}} onClick={getDataProducts}> нажми</button>
-    </div>
+      <div style={{margin: '0px 50px'}}>
+        <h2>Головна</h2>
+        <button
+            style={{border: '1px solid black', padding: '5px 10px', marginRight: '5px'}}
+            onClick={getProductData}>
+          Get Category list log
+        </button>
+        <button
+            style={{border: '1px solid black', padding: '5px 10px', marginLeft: '5px'}}
+            onClick={getUserData}>
+          Get User list log
+        </button>
+      </div>
   )
 }
