80c8e8bd7054d980c8ee87b29d6736cefb2eff4d
ALexus <sumskoj01@gmail.com>
2023-05-16 20:59:52 +0300

    Style fix

--- src/ui/NavbarHeader.jsx
+++ src/ui/NavbarHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NavbarHeader = () => {
   return (
-    <div className='text-7xl'>
+    <div className='text-xl'>
         <div>
             <Link to="/"> Головна</Link> 
             <Link to="/checkout"> Оформити замовлення</Link>
--- src/ui/ProductSearch.jsx
+++ src/ui/ProductSearch.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
+import Product from "./Product.jsx";
 
 const ProductSearch = () => {
-    const [searchQuery, setSearchQuery] = useState('');
+    const [query, setQuery] = useState('');
     const [products, setProducts] = useState([]);
 
-    const handleSearch = async (e) => {
+    const getData = async (e) => {
         e.preventDefault();
 
         try {
-            if (!searchQuery.trim()) {
+            if (!query.trim()) {
                 setProducts([]);
                 return;
             }
 
             const response = await axios.get('https://etech-5fydkirpga-lm.a.run.app/products/all', {
-                params: { title: searchQuery, producer: searchQuery }
+                params: {title: query, producer: query}
             });
 
             setProducts(response.data);
@@ -24,39 +25,47 @@ const ProductSearch = () => {
         }
     };
 
-    const handleProductClick = (product) => {
-        setSelectedProduct(product);
-        window.location.href = `/products/${product.id}`;
-    };
-
     const handleInputChange = (e) => {
         const query = e.target.value;
-        setSearchQuery(query);
+        setQuery(query);
         if (!query.trim()) {
             setProducts([]);
         }
     };
 
     return (
-        <div>
-            <form onSubmit={handleSearch}>
-                <input
-                    type="text"
-                    value={searchQuery}
-                    onChange={handleInputChange}
-                    placeholder="Search for a product"
-                />
+        <div className='mx-auto my-5 w-2/3'>
+            <form onSubmit={getData}>
+                <label htmlFor="default-search"
+                       className="mb-2 text-sm font-medium text-gray-900 sr-only">
+                    Search
+                </label>
+                <div className='relative'>
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+                             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                    <input type="text"
+                           id="default-search"
+                           className="block w-full p-3 pl-10 text-black border border-gray-300 rounded-lg bg-gray-50"
+                           placeholder="Search Mockups, Logos..."
+                           onChange={handleInputChange}
+                           required
+                    />
+                    <button type="submit"
+                            className="text-black absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-[black] font-medium rounded-lg px-4 py-2">
+                        Search
+                    </button>
+                </div>
             </form>
 
             {products.length > 0 && (
                 <div>
                     {products.map((product) => (
-                        <div key={product.id} onClick={() => handleProductClick(product)}>
-                            <h3>{product.title}</h3>
-                            <p>Price: {product.price}</p>
-                            <p>Producer: {product.producer}</p>
-                            {/* Render other product details here */}
-                        </div>
+                        <Product product={product} />
                     ))}
                 </div>
             )}
