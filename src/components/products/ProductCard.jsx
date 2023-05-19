b9dc6d6047c8738b70770403dc3831e424d7ee07
ALexus <sumskoj01@gmail.com>
2023-05-19 20:09:30 +0300

    Fetching images from GCP + uploading

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ product }) => {
       <div className="text-start p-10">
         <div className="w-[200px] h-[280px] border-2 border-black/20 rounded-[3px]">
           <div className="w-[120px] h-[130px] mx-auto flex items-center justify-center pt-2 ">
-            <img src={`http://localhost:8080/images/product/${product.imgPath}`} alt={product.title} />
+            <img src={`https://etech-5fydkirpga-lm.a.run.app/images/product/${product.imgPath}`} alt={product.title} />
           </div>
           <div className="mt-[20px] py-2">
             <h3 className="text-[14px]">{product.title.slice(0, 40)} ...</h3>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,12 +9,29 @@ export function HomePage() {
         setSelectedFile(event.target.files[0]);
     };
 
-    const handleUpload = () => {
+    const handleUploadProduct = () => {
         if (selectedFile) {
             const formData = new FormData();
             formData.append('file', selectedFile);
 
             axios.post('https://etech-5fydkirpga-lm.a.run.app/images/product', formData)
+                .then((response) => {
+                    console.log(response.status);
+                    // Добавьте здесь необходимую логику после успешной загрузки файла
+                })
+                .catch((error) => {
+                    console.error('Error uploading file:', error);
+                    // Добавьте здесь необходимую обработку ошибок
+                });
+        }
+    };
+
+    const handleUploadCategory = () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+
+            axios.post('https://etech-5fydkirpga-lm.a.run.app/images/category', formData)
                 .then((response) => {
                     console.log(response.status);
                     // Добавьте здесь необходимую логику после успешной загрузки файла
@@ -33,8 +50,14 @@ export function HomePage() {
             <Category/>
 
             <div>
+                <p>Называть id.png (где id - айдишник продукта)</p>
                 <input type="file" onChange={handleFileChange}/>
-                <button onClick={handleUpload}>Upload</button>
+                <button onClick={handleUploadProduct}>Upload Product (.png)</button>
+            </div>
+            <div>
+                <p>Называть id.svg</p>
+                <input type="file" onChange={handleFileChange}/>
+                <button onClick={handleUploadCategory}>Upload Category (.svg)</button>
             </div>
 
         </div>
