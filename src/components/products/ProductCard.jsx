8c5320ae101dc51ededfc8064e3e3ed1f24e6c5e
skinner.x8 <Cezar21044@gmail.com>
2023-05-20 01:24:25 +0300

    Products

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product}) => {
+  function getPriceWithDiscount(price, discount) {
+    return discount !== 0 ? price - price / 100 * discount : price;
+}
 
   if (!product) {
     return <div>Loading...</div>;
@@ -9,16 +12,20 @@ const ProductCard = ({ product }) => {
 
   return (
     <Link to={`/products/id/id=${product.id}`}>
-      <div className="text-start p-10">
-        <div className="w-[200px] h-[280px] border-2 border-black/20 rounded-[3px]">
-          <div className="w-[120px] h-[130px] mx-auto flex items-center justify-center pt-2 ">
-            <img src={`https://etech-5fydkirpga-lm.a.run.app/images/product/${product.imgPath}`} alt={product.title} />
-          </div>
-          <div className="mt-[20px] py-2">
-            <h3 className="text-[14px]">{product.title.slice(0, 40)} ...</h3>
-            <p className="font-[700] text-[18px] my-2">Price: {product.price} $</p>
-          </div>
+      <div className="mx-auto px-5 py-8 border-2 border-black/30 gap-4 h-[100%]">
+        <div className="w-full overflow-clip">
+          <img
+            src={`https://etech-5fydkirpga-lm.a.run.app/images/product/${product.imgPath}`}
+            alt={product.title}
+            className="w-full object-cover object-center group-hover:opacity-75"
+          />
         </div>
+        <p className="mt-4 text-[16px] text-gray-700 truncate">
+          {product.title}
+        </p>
+        <p className={product.discount >= 1 ? 'mt-2 text-[22px] font-bold text-[#f84147]' : 'hidden'}>{getPriceWithDiscount(product.price, product.discount)} ₴ 
+        </p>
+        <p className={product.discount > 0 ? 'font-[400] text-[16px] text-black/80 line-through ' : "font-[700] text-[24px]"}>{product.price} ₴</p>
       </div>
     </Link>
   );
